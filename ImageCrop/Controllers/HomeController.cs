b7665b7e4f40aea03d3f3ba5d9cf8f33378b0ca4
Maniac Звинаревский <maks-bot-5@mail.ru>
2017-02-14 16:08:05 +0200

    CropImage

--- ImageCrop/Controllers/HomeController.cs
+++ ImageCrop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
 
 namespace ImageCrop.Controllers
 {
@@ -12,19 +13,27 @@ namespace ImageCrop.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index( FormCollection form)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var x = Convert.ToInt32(form["X"]);
+            var y = Convert.ToInt32(form["Y"]);
+            var w = Convert.ToInt32(form["W"]);
+            var h = Convert.ToInt32(form["H"]);
+            string sourseFile = Request.MapPath("~/Image/funny_cat_v1.jpg");
+            Image oImage = Image.FromFile(sourseFile);
+            var bmp = new Bitmap(w, h, oImage.PixelFormat);
+            var g = Graphics.FromImage(bmp);
+            g.DrawImage(oImage, new Rectangle(0, 0, w, h),
+                new Rectangle(x, y, w, h), GraphicsUnit.Pixel);
+            System.Drawing.Imaging.ImageFormat frm = oImage.RawFormat;
+            oImage.Dispose();
+            string destFile = Request.MapPath("~/Upload/funny_cat_v1.jpg");
+            bmp.Save(destFile, frm);
+            return RedirectToAction("Index");
         }
+        
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      
     }
 }
