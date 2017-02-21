322944314046b8e07c75d43d6f97f0ee7f2dc667
Maniac Звинаревский <maks-bot-5@mail.ru>
2017-02-21 15:34:12 +0200

    кроп

--- ImageCrop/Controllers/HomeController.cs
+++ ImageCrop/Controllers/HomeController.cs
@@ -4,36 +4,83 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Drawing;
+using System.Net;
+using System.IO;
 
 namespace ImageCrop.Controllers
 {
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult Index( FormCollection form)
+
+        public ActionResult About()
         {
-            var x = Convert.ToInt32(form["X"]);
-            var y = Convert.ToInt32(form["Y"]);
-            var w = Convert.ToInt32(form["W"]);
-            var h = Convert.ToInt32(form["H"]);
-            string sourseFile = Request.MapPath("~/Image/funny_cat_v1.jpg");
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        /// <summary>
+        /// С   
+        /// </summary>
+        /// <returns></returns>
+        public class Cordinats
+        {
+            public int CorX { get; set; }
+            public int CorY { get; set; }
+            public int CorW { get; set; }
+            public int CorH { get; set; }
+            public string nameimg { get; set; }
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            if (Request.Files == null)
+            {
+                return Json("Не удалось загрузить файл");
+            }
+            var upload = Request.Files[0];
+            var path = Server.MapPath("~/Files/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            // получаем имя файла
+            string fileName = Path.GetFileName(upload.FileName);
+            // сохраняем файл в папку Files в проекте
+            upload.SaveAs(Server.MapPath("~/Files/" + fileName));
+            return Json(fileName);
+        }
+        [HttpPost]
+        public JsonResult Crop(Cordinats cor)
+        {
+            // получаем имя файла
+            string sourseFile = Request.MapPath("~/Files/" + cor.nameimg);
             Image oImage = Image.FromFile(sourseFile);
-            var bmp = new Bitmap(w, h, oImage.PixelFormat);
+            var bmp = new Bitmap(cor.CorW, cor.CorH, oImage.PixelFormat);
             var g = Graphics.FromImage(bmp);
-            g.DrawImage(oImage, new Rectangle(0, 0, w, h),
-                new Rectangle(x, y, w, h), GraphicsUnit.Pixel);
+            g.DrawImage(oImage, new Rectangle(0, 0, cor.CorW, cor.CorH),
+                new Rectangle(cor.CorX, cor.CorY, cor.CorW, cor.CorH), GraphicsUnit.Pixel);
             System.Drawing.Imaging.ImageFormat frm = oImage.RawFormat;
             oImage.Dispose();
-            string destFile = Request.MapPath("~/Upload/funny_cat_v1.jpg");
+            string destFile = Request.MapPath("~/Files/" + cor.nameimg);
             bmp.Save(destFile, frm);
-            return RedirectToAction("Index");
-        }
-        
 
-      
+
+            return Json("Успешно обрезано");
+        }
+
+
     }
 }
